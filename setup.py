299e1b0ae9d829c039aa7556fff31b32f89f1b40
bernharl <b.n.lotsberg@fys.uio.no>
2020-12-04 20:25:45 +0100

    Fix setup.py??v8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 
-PACKAGE_NAME = 'camelsml'
+PACKAGE_NAME = "camelsml"
 
 folder = Path(__file__).parent
 
@@ -10,27 +10,27 @@ version = {}
 with open(folder / PACKAGE_NAME / "version.py") as fp:
     exec(fp.read(), version)
 
-packs = find_packages(where=folder, exclude=('tests*',))
+packs = find_packages(where=folder, exclude=("tests*",))
 
-with open(folder / 'requirements.txt') as fp:
+with open(folder / "requirements.txt") as fp:
     requirements = fp.read()
 
-with open('README.md') as fp:
+with open("README.md") as fp:
     readme = fp.read()
 
-with open('LICENSE') as fp:
+with open("LICENSE") as fp:
     license = fp.read()
 
 setup(
     name=PACKAGE_NAME,
-    version=version['__version__'],
-    description='Machine learning for CAMELS_GB (support for more in the future maybe)',
+    version=version["__version__"],
+    description="Machine learning for CAMELS_GB (support for more in the future maybe)",
     long_description=readme,
-    url='https://github.com/epigramai/videostream',
-    author='Frederik Kratzert (original code), Bernhard Nornes Lotsberg (forked code)',
-    author_email='bernhardnorneslotsberg@gmail.com',
+    url="https://github.com/epigramai/videostream",
+    author="Frederik Kratzert (original code), Bernhard Nornes Lotsberg (forked code)",
+    author_email="bernhardnorneslotsberg@gmail.com",
     license=license,
     packages=packs,
-    package_dir={'': camelsml},
+    package_dir={"": "camelsml"},
     install_requires=requirements,
 )
