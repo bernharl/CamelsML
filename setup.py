df82648c566daeac0ca565e88e80295960c9888e
bernharl <b.n.lotsberg@fys.uio.no>
2020-12-04 20:17:48 +0100

    Fix setup.py??v5

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
-from setuptools import setup
-import pathlib
+from pathlib import Path
+from setuptools import setup, find_packages
 
-requirements = pathlib.Path("requirements.txt").read_text().splitlines()
 
+PACKAGE_NAME = 'camelsml'
+
+folder = Path(__file__).parent
+
+version = {}
+with open(folder / PACKAGE_NAME / "version.py") as fp:
+    exec(fp.read(), version)
+
+packs = find_packages(where=folder, exclude=('tests*',))
+
+with open(folder / 'requirements.txt') as fp:
+    requirements = fp.read()
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+with open('LICENCE') as fp:
+    license = fp.read()
 
 setup(
+    name=PACKAGE_NAME,
+    version=version['__version__'],
+    description='Machine learning for CAMELS_GB (support for more in the future maybe)',
+    long_description=readme,
+    url='https://github.com/epigramai/videostream',
+    author='Frederik Kratzert (original code), Bernhard Nornes Lotsberg (forked code)',
+    author_email='bernhardnorneslotsberg@gmail.com',
+    license=license,
+    packages=packs,
+    package_dir={'': SRC_FOLDER},
     install_requires=requirements,
 )
